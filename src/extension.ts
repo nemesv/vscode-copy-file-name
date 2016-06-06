c59b665e1222dc1d4da6fcec36c4698d881c62cf
Viktor Nemes <nemesv@gmail.com>
2016-06-06 21:51:03 +0200

    Implement the three copy to clipboard commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,38 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as copyPaste from 'copy-paste';
+import * as path from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "copy-file-name" is now active!');
+    let disposable = vscode.commands.registerCommand('copy-file-name.copyFileName', () => {
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+        var fullPath = vscode.window.activeTextEditor.document.fileName;
+        var extName = path.extname(fullPath);
+        var fileName = path.basename(fullPath, extName);
+        copyPaste.copy(fileName);
+        vscode.window.showInformationMessage(`The filename "${fileName}" was copied to the clipboard.`);
+    });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    context.subscriptions.push(disposable);
+    
+    disposable = vscode.commands.registerCommand('copy-file-name.copyFileNameWithExtension', () => {
+
+        var fullPath = vscode.window.activeTextEditor.document.fileName;
+        var fileName = path.basename(fullPath);
+        copyPaste.copy(fileName);
+        vscode.window.showInformationMessage(`The filename "${fileName}" was copied to the clipboard.`);
+    });
+
+    context.subscriptions.push(disposable);
+    
+    disposable = vscode.commands.registerCommand('copy-file-name.copyFullPath', () => {
+
+        var fullPath = vscode.window.activeTextEditor.document.fileName;
+        var fileName = fullPath;
+        copyPaste.copy(fileName);
+        vscode.window.showInformationMessage(`The filename "${fileName}" was copied to the clipboard.`);
     });
 
     context.subscriptions.push(disposable);
