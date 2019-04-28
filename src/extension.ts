caf21700ee441c73b11030677e7935f7c0126405
Viktor Nemes <nemesv@gmail.com>
2019-04-28 13:11:56 +0200

    Include the copy commands in the editor title and the explorer context menu

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,24 @@ import * as vscode from 'vscode';
 import * as copyPaste from 'copy-paste';
 import * as path from 'path';
 
+let getPath = function(args) {
+    let path = null;
+    if (args && args.length > 0) {
+        path = args[0].fsPath;
+    }
+    if (!path)
+        path =  vscode.window.activeTextEditor.document.fileName;
+    return path;
+}
 
-let pasteAndShowMessage= function(fileName: string) {
+let pasteAndShowMessage = function(fileName: string) {
     copyPaste.copy(fileName);
     vscode.window.setStatusBarMessage(`The filename "${fileName}" was copied to the clipboard.`, 3000);
 }
 
-
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('copy-file-name.copyFileName', () => {
-        var fullPath = vscode.window.activeTextEditor.document.fileName;
+    let disposable = vscode.commands.registerCommand('copy-file-name.copyFileName', (...args) => {  
+        var fullPath = getPath(args);
         var extName = path.extname(fullPath);
         var fileName = path.basename(fullPath, extName);
         pasteAndShowMessage(fileName);
@@ -21,9 +29,8 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable);
     
-    disposable = vscode.commands.registerCommand('copy-file-name.copyFileNameWithExtension', () => {
-
-        var fullPath = vscode.window.activeTextEditor.document.fileName;
+    disposable = vscode.commands.registerCommand('copy-file-name.copyFileNameWithExtension', (...args) => {
+        var fullPath = getPath(args);
         var fileName = path.basename(fullPath);
         pasteAndShowMessage(fileName);
     });
